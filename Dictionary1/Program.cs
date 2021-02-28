79ff29c752c465806cf57b65140e5db50730983b
rabia.dzky@hotmail.com <rabia.dzky@hotmail.com>
2021-02-28 17:38:45 +0300

    Add project files.

---
+++ Dictionary1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ayşe");
+            myDictionary.Add(2, "Rabia");
+            myDictionary.Add(3, "Selin");
+            myDictionary.DictionaryList();
+        }
+    }
+}
